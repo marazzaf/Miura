38047854849f4aaf8cf636e35b82cd87e008421c
Frederic Marazzato <marazzato@lsu.edu>
2021-01-08 16:39:15 -0600

    Where does the problem come from?

--- no_constraint.py
+++ no_constraint.py
@@ -3,6 +3,7 @@
 from dolfin import *
 import numpy as np
 import ufl
+import sys
 
 size_ref = 5 #debug
 L,l = 1,1
@@ -10,27 +11,38 @@ mesh = RectangleMesh(Point(-L/2,-l/2), Point(L/2, l/2), size_ref, size_ref, "cro
 bnd = MeshFunction('size_t', mesh, 1)
 bnd.set_all(0)
 
-V = FunctionSpace(mesh, 'CG', 1)
-W = VectorFunctionSpace(mesh, 'CG', 1)
+#V = FunctionSpace(mesh, 'CG', 1)
+#W = VectorFunctionSpace(mesh, 'CG', 1)
 
-phi_x = Function(V)
-phi_y = Function(V)
-phi = as_vector((phi_x, phi_y))
-#phi = Function(W)
-v = TestFunction(V)
+#Créer un espace mixte avec V et W pour pouvoir résoudre?
+Ve = VectorElement('CG',  mesh.ufl_cell(), 1)
+We = FiniteElement('CG',  mesh.ufl_cell(), 1)
+M = MixedElement(Ve, We)
+M = FunctionSpace(mesh, M)
+V = M.sub(0).collapse()
+W = M.sub(1).collapse()
+
+#Creating the functions to define the bilinear form
+#tot = Function(M)
+#phi,truc = tot.split()
+#bidule,psi = TestFunctions(M)
+phi = Function(V)
+psi = TestFunction(W)
 
 #Dirichlet BC
 R = 10 #radius of the outer circle
 x = SpatialCoordinate(mesh)
 theta = ufl.atan_2(x[0], x[1])
-#phi = Expression(('R*cos(', 'R*'), R=R, degree = 2)
+#phi_D = Expression(('R*cos(', 'R*'), R=R, degree = 2)
 phi_D = as_vector((R*cos(theta), R*sin(theta)))
+#test = Expression('atanh(x[0])', degree=3) #atanh bien def en C
+#comment s'en servir pour avoir la fonction ufl?
 
 #creating the bc object
-#bc = DirichletBC(W, phi_D, bnd, 0)
-bc_1 = DirichletBC(V, phi_D[0], bnd, 0)
-bc_2 = DirichletBC(V, phi_D[1], bnd, 0)
-bc = [bc_1, bc_2]
+bc = DirichletBC(V, phi_D, bnd, 0)
+#bc_1 = DirichletBC(M.sub(0).sub(0), phi_D[0], bnd, 0)
+#bc_2 = DirichletBC(M.sub(0).sub(1), phi_D[1], bnd, 0)
+#bc = [bc_1, bc_2]
 
 #Writing energy. No constraint for now...
 norm_phi_x = sqrt(inner(phi.dx(0), phi.dx(0)))
@@ -40,8 +52,10 @@ f = as_vector((2*ufl.atan(0.5*norm_phi_x), -4/norm_phi_y))
 
 
 #bilinear form
-a = inner(f, grad(v)) * dx
+#a = inner(f, grad(psi)) * dx
+#a = inner(phi, grad(psi)) * dx - psi*dx
+a = inner(as_vector((2*ufl.atan(0.5*norm_phi_x),norm_phi_y)), grad(psi)) * dx - psi * dx
 
 #solving problem
-solve(a == 0, phi_x, bc, solver_parameters={"newton_solver":{"relative_tolerance":1e-6}})
+solve(a == 0, phi, bc, solver_parameters={"newton_solver":{"relative_tolerance":1e-6}})
 
