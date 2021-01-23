f3538c3fc7361b29b39ad59cf3212517124ac4db
Frederic Marazzato <marazzato@lsu.edu>
2021-01-22 18:37:36 -0600

    Bug when adding rhs for Neumann BC. Where does that come from?

--- constrained.py
+++ constrained.py
@@ -1,15 +1,14 @@
 #coding: utf-8
 
-from fenics import *
+from dolfin import *
 from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np 
-import ufl
 import sys
 
 #To determine domain
 theta = pi/2
-L = Constant(2*sin(0.5*acos(0.5/cos(0.5*theta))))
+L = 2*sin(0.5*acos(0.5/cos(0.5*theta))) #Constant
 l = 2*pi
 size_ref = 10 #degub: 5
 Nx,Ny = int(size_ref*l/float(L)),size_ref
@@ -65,15 +64,15 @@ norm_phi_x = sqrt(inner(phi.dx(0), phi.dx(0)))
 norm_phi_y = sqrt(inner(phi.dx(1), phi.dx(1)))
 
 #bilinear form
-#a1 = ufl.ln((1+0.5*norm_phi_x)/(1-0.5*norm_phi_x)) * (psi[0].dx(0)+psi[1].dx(0)+psi[2].dx(0)) * dx #correct on
-a1 = ufl.ln(abs((1+0.5*norm_phi_x)/(1-0.5*norm_phi_x))) * (psi[0].dx(0)+psi[1].dx(0)+psi[2].dx(0)) * dx #test
+a1 = ln((1+0.5*norm_phi_x)/(1-0.5*norm_phi_x)) * (psi[0].dx(0)+psi[1].dx(0)+psi[2].dx(0)) * dx #correct
+#a1 = ln(abs((1+0.5*norm_phi_x)/(1-0.5*norm_phi_x))) * (psi[0].dx(0)+psi[1].dx(0)+psi[2].dx(0)) * dx #test
 a2 = -4/norm_phi_y * (psi[0].dx(1)+psi[1].dx(1)+psi[2].dx(1)) * dx
 a = a1 + a2
 
 #adding equality constraint with penalty
 pen = 1e5
 b = pen * ((1 - 0.25*norm_phi_x) * norm_phi_y - 1)**2 * dx #least-squares penalty on equality constraint
-c = derivative(b, phi)
+c = derivative(b, phi, psi)
 
 #To add the inequality constraints
 def ppos(x): #definition of positive part for inequality constraints
@@ -82,31 +81,45 @@ def ppos(x): #definition of positive part for inequality constraints
 #adding inequality constraint with penalty
 #d = pen * (ppos(norm_phi_x**2 - 3) + ppos(norm_phi_y**2 - 4) + ppos(1 - norm_phi_y**2)) * dx
 d = pen * (ppos(norm_phi_x - sqrt(3))**2 + ppos(norm_phi_y - 2)**2 + ppos(1 - norm_phi_y)**2) * dx
-e = derivative(d, phi)
+e = derivative(d, phi, psi)
 
 #Adding Neumann BC
 neumann_x = phi_D.dx(0)
+test = grad(phi_D)
+#print(test[0,0])
+#print(test[0,1])
+#print(test[2,0])
+#sys.exit()
+#neumann_x = as_vector((Dx(phi_D[0],0),Dx(phi_D[1],0),Dx(phi_D[2],0)))
 neumann_y = phi_D.dx(1)
-rhs = (inner(neumann_x, psi) + inner(neumann_y, psi)) * (ds(2) + ds(3))
+#neumann_x = test[:,0]
+#print(neumann_x)
+#neumann_y = test[:,1]
+rhs = (dot(neumann_x, psi) + dot(neumann_y, psi)) * ds(2)# + ds(3))
+#rhs = (phi_D[0]*psi[0]) * ds(2)
 
 tot = a + c + e - rhs
+#F = action(tot, phi)
 #tot = a #only minimal surface for now
 
-#Other solver
-#dphi = TrialFunction(V)
-J = derivative(tot, phi) #, dphi)
-problem = NonlinearVariationalProblem(tot, phi, bcs, J)
-solver  = NonlinearVariationalSolver(problem)
+# Compute solution
+solve(tot == 0, phi, bcs, solver_parameters={"newton_solver":{"relative_tolerance":1e-6}})
 
-#Parameters
-prm = solver.parameters
-#info(prm, True) #to get info on parameters
-prm["nonlinear_solver"] = "newton" #"snes" #"newton"
-prm["newton_solver"]['relative_tolerance'] = 1e-6
-prm['newton_solver']['maximum_iterations'] = 100
-
-#Solving
-solver.solve()
+##Other solver
+##dphi = TrialFunction(V)
+#J = derivative(tot, phi) #, dphi)
+#problem = NonlinearVariationalProblem(tot, phi, bcs, J)
+#solver  = NonlinearVariationalSolver(problem)
+#
+##Parameters
+#prm = solver.parameters
+##info(prm, True) #to get info on parameters
+#prm["nonlinear_solver"] = "newton" #"snes" #"newton"
+#prm["newton_solver"]['relative_tolerance'] = 1e-6
+#prm['newton_solver']['maximum_iterations'] = 100
+#
+##Solving
+#solver.solve()
 
 #solution verifies constraints?
 ps = inner(phi.dx(0), phi.dx(1)) * dx
