a2c1991f2825adb7fd9a28dcd359a79b4d079a38
Frederic Marazzato <marazzato@lsu.edu>
2021-02-18 14:30:10 -0600

    Find out why the discrete solution does not give zero...

--- alternate.py
+++ alternate.py
@@ -10,7 +10,7 @@ import sys
 theta = pi/2
 L = 2*sin(0.5*acos(0.5/cos(0.5*theta)))
 l = 2*pi
-size_ref = 5 #degub: 5
+size_ref = 2 #degub: 5
 Nx,Ny = int(size_ref*l/float(L)),size_ref
 mesh = RectangleMesh(Point(-L/2,0), Point(L/2, l), Nx, Ny, "crossed")
 bnd = MeshFunction('size_t', mesh, 1)
@@ -21,6 +21,7 @@ ds = ds(subdomain_data=bnd)
 V = VectorFunctionSpace(mesh, 'CG', 2, dim=3)
 phi = Function(V, name="surface")
 psi = TestFunction(V)
+dphi = TrialFunction(V)
 
 #Defining the boundaries
 def top_down(x, on_boundary):
@@ -65,15 +66,20 @@ for bc in bcs:
 norm_phi_x = sqrt(inner(phi.dx(0), phi.dx(0)))
 norm_phi_y = sqrt(inner(phi.dx(1), phi.dx(1)))
 
+##test
+#truc = assemble(inner(phi.dx(0), psi) * dx)
+#truc_bis = assemble((dphi[0].dx(0) * psi[0] + dphi[1].dx(0) * psi[1] + dphi[2].dx(0) * psi[2]) * dx)
+#print(as_backend_type(truc - truc_bis).array().nonzero())
+#sys.exit()
+
 #bilinear form
-a1 = (phi[0].dx(0).dx(0)*psi[0] + phi[1].dx(0).dx(0)*psi[1] + phi[2].dx(0).dx(0)*psi[2]) / (1 - 0.25*norm_phi_x**2) * dx
-a2 = 4/norm_phi_y**2 * (phi[0].dx(1).dx(1)*psi[0] + phi[1].dx(1).dx(1)*psi[1] + phi[2].dx(1).dx(1)*psi[2]) * dx
+a1 = inner(phi.dx(0).dx(0), psi) / (1 - 0.25*norm_phi_x**2) * dx
+a2 = 4/norm_phi_y**2 * inner(phi.dx(1).dx(1), psi) * dx
 a = a1 + a2
 
-##old one
-#a1 = ln(abs((1+0.5*norm_phi_x)/(1-0.5*norm_phi_x))) * (psi[0].dx(0)+psi[1].dx(0)+psi[2].dx(0)) * dx #correct
-#a2 = -4/norm_phi_y * (psi[0].dx(1)+psi[1].dx(1)+psi[2].dx(1)) * dx
-#a = a1+a2
+#test
+test = assemble(a)
+print(test[:].sum())
 
 #adding equality constraint with penalty
 pen = 1e5
@@ -121,14 +127,6 @@ tot = a
 #
 ##Solving
 #solver.solve() 
-
-##solution is okay?
-##test = action(a, Constant((1,1,1)))
-#test = assemble(a)
-#test = assemble( as_vector((phi[0].dx(0).dx(0), phi[1].dx(0).dx(0), phi[2].dx(0).dx(0))) / (1 - 0.25*norm_phi_x**2) * dx + 4/norm_phi_y**2 * as_vector((phi[0].dx(1).dx(1), phi[1].dx(1).dx(1), phi[2].dx(1).dx(1))) * dx )
-#print(test)
-##print(test)
-#sys.exit()
 
 #Old
 a1 = ln(abs((1+0.5*norm_phi_x)/(1-0.5*norm_phi_x))) * (psi[0].dx(0)+psi[1].dx(0)+psi[2].dx(0)) * dx #correct
