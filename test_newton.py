2a6703dac31a9680a95ccbfc3a9bf6c167dfa010
Frederic Marazzato <marazzato@lsu.edu>
2021-02-25 10:36:44 -0600

    The version without ineq constraints and the Newton solver works with Full dirichlet BC and the analytical solution as initial guess. See what happens with mixed BC and another Initial Guess.

--- test_newton.py
+++ test_newton.py
@@ -17,7 +17,6 @@ size_ref = 25 #degub: 5
 Nx,Ny = int(size_ref*l/float(L)),size_ref
 mesh = RectangleMesh(Point(-L/2,0), Point(L/2, l), Nx, Ny, "crossed")
 V = VectorFunctionSpace(mesh, 'Lagrange', 1, dim=3)
-U = FunctionSpace(mesh, 'Lagrange', 1)
 
 # initial guess (its boundary values specify the Dirichlet boundary conditions)
 z = Expression('2*sin(theta/2)*x[0]', theta=theta, degree = 5)
@@ -29,14 +28,25 @@ phi_old = interpolate(phi_D, V)
 
 # Define nonlinear weak formulation
 tol = 1.e-6
-u = u0  # most recently computed solution
-v = TestFunction(V)
-F = q(u) * inner(grad(u), grad(v)) * dx
+phi = phi_old
+psi = TestFunction(V)
+F = (p(phi) * inner(psi.dx(0), phi.dx(0)) + q(phi) * inner(psi.dx(1), phi.dx(1))) * dx
+
+#Adding pen
+#Useful?
+
 # solve nonlinear problem using Newton's method.  Note that there
 # are numerous parameters that can be used to control the Newton iteration
-solve(F == 0, u, DirichletBC(V, u0exp, DomainBoundary()), \
-              solver_parameters={"newton_solver": {"absolute_tolerance": tol}})
+solve(F == 0, phi, DirichletBC(V, phi_D, DomainBoundary()), solver_parameters={"newton_solver": {"absolute_tolerance": tol}})
 
-print('surface area:', assemble(sqrt(1+inner(grad(u),grad(u)))*dx))
-plot(u) #, interactive=True)
+#plotting solution
+vec_phi_ref = phi.vector().get_local()
+vec_phi = vec_phi_ref.reshape((3, len(vec_phi_ref) // 3))
+vec_phi_aux = vec_phi_ref.reshape((len(vec_phi_ref) // 3, 3))
+
+#3d plot
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+for i in vec_phi_aux:
+  ax.scatter(i[0], i[1], i[2], color='r')
 plt.show()
