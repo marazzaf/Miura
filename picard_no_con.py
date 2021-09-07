01d61c223014a1d9e996cd55a391287fa7dd005a
Frederic Marazzato <marazzato@lsu.edu>
2021-09-07 08:35:34 -0500

    Looks like the most simple bilinear form does not give the expected result. Try with the broken elements of Smears?

--- picard_no_con.py
+++ picard_no_con.py
@@ -6,17 +6,18 @@ import ufl
 
 # the coefficient functions
 def p(phi):
-  return  inner(phi.dx(0), phi.dx(0))**2
+  #return  inner(phi.dx(0), phi.dx(0))**2
+  return  1 / (1 - 0.25 * inner(phi.dx(0), phi.dx(0)))
 
 def q(phi):
-  return 4
+  return 4 / inner(phi.dx(1), phi.dx(1))
 
 # Create mesh and define function space
 theta = pi/2
 L = 2*sin(0.5*acos(0.5/cos(0.5*theta)))
 alpha = sqrt(1 / (1 - sin(theta/2)**2))
 l = 2*pi/alpha
-size_ref = 5 #10 #degub: 5
+size_ref = 10 #10 #degub: 5
 Nx,Ny = int(size_ref*l/float(L)),size_ref
 mesh = RectangleMesh(Point(0,0), Point(L, l), Nx, Ny, "crossed")
 V = VectorFunctionSpace(mesh, 'CG', 5, dim=3) #degree 1
@@ -33,7 +34,7 @@ phi_old = interpolate(phi_D, V)
 phi = Function(V)
 phi_t = TrialFunction(V)
 psi = TestFunction(V)
-a = inner(p(phi_old) * phi_t.dx(0).dx(0) + 4*phi_t.dx(1).dx(1), div(grad(psi))) * dx #test
+a = inner(p(phi_old) * phi_t.dx(0).dx(0) + q(phi_old)*phi_t.dx(1).dx(1), div(grad(psi))) * dx #test
 L = Constant(0.)*psi[0]*dx
 
 # Picard iteration
@@ -56,7 +57,7 @@ for iter in range(maxiter):
   #plt.show()
   #sys.exit()
     
-  eps = sqrt(abs(assemble(inner(grad(phi-phi_old),grad(phi-phi_old))*dx))) # check increment size as convergence test
+  eps = sqrt(assemble(inner(grad(phi-phi_old),grad(phi-phi_old))*dx)) # check increment size as convergence test
   #area = assemble(sqrt(1+inner(grad(u),grad(u)))*dx)
   print('iteration{:3d}  H1 seminorm of delta: {:10.2e}'.format(iter+1, eps))
   if eps < tol:
