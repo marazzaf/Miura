188e1f23c98830750c762e2aea225220e72e76ce
marazzaf <frederic.marazzato@ponts.org>
2021-10-21 16:53:38 -0500

    Seems to work but no convergence on the equality constraint. Add a penalty term about that?

--- new_shape.py
+++ new_shape.py
@@ -23,13 +23,32 @@ def sq_norm(f):
 theta = np.pi/2
 ##check the sizes of the mesh. Might be the problem with some constraints.
 L = 2*np.sin(0.5*np.arccos(0.5/np.cos(0.5*theta))) #length of rectangle
-#print(L)
 alpha = np.sqrt(1 / (1 - np.sin(theta/2)**2))
 H = 2*np.pi/alpha #height of rectangle
-#print(H)
+l = np.sin(theta/2)*L #total height of cylindre
+modif = 0.1 #0.02 #variation at the top
+
+#writing the matrix of the system
+A = np.zeros((6,6))
+#Filling-in by line
+A[0,:] = np.array([L**2/4, 0, 0, -L/2, 0, 1])
+A[1,:] = np.array([L**2/4, H**2/4, L*H/4, L/2, H/2, 1])
+A[2,:] = np.array([L**2/4, 0, 0, L/2, 0, 1])
+A[3,:] = np.array([0, H**2/4, 0, 0, H/2, 1])
+A[4,:] = np.array([L**2/4, H**2, -L*H/2, -L/2, H, 1])
+A[5,:] = np.array([L**2/4, H**2, L*H/2, L/2, H, 1])
+
+#Corresponding right-hand side
+b = np.array([-l, l, l*(1+modif), 0, -l, l*(1-modif)])
+
+#solution
+coeffs = np.linalg.solve(A,b)
+
+def z(x,y):
+    return coeffs[0]*x*x + coeffs[1]*y*y + coeffs[2]*x*y + coeffs[3]*x + coeffs[4]*y + coeffs[5]
 
 #Loading mesh
-num_computation = 1
+num_computation = 2
 mesh = Mesh('rectangle_%i.msh' % num_computation) #change mesh to not use the symmetry any longer
 V = VectorFunctionSpace(mesh, "HER", 3, dim=3)
 
@@ -38,7 +57,7 @@ U = VectorFunctionSpace(mesh, 'CG', 1, dim=3)
 
 # Boundary conditions
 x = SpatialCoordinate(mesh)
-z = -6.4e-3*x[0]*x[1] + 1.43*x[0] - 2.4e-3*x[1] + 5.4e-3
+z = z(x[0], x[1])
 rho = sqrt(4*np.cos(theta/2)**2*x[0]*x[0] + 1)
 phi_D = as_vector((rho*cos(alpha*x[1]), rho*sin(alpha*x[1]), z))
 
@@ -67,13 +86,13 @@ a = inner(p(phi) * phi_t.dx(0).dx(0) + q(phi)*phi_t.dx(1).dx(1), div(grad(psi)))
 
 #penalty to impose Dirichlet BC
 h = CellDiameter(mesh)
-pen = 1e1
-pen_term = pen/h**4 * inner(phi_t, psi) * (ds(1) + ds(3))
+pen = 1e2
+pen_term = pen/h**4 * inner(phi_t, psi) * ds #(ds(1) + ds(3))
 a += pen_term
-L = pen/h**4 * inner(phi_D, psi)  * (ds(1) + ds(3))
+L = pen/h**4 * inner(phi_D, psi)  * ds #(ds(1) + ds(3))
 
 #penalty for inequality constraint
-pen = 1e1
+#pen = 1e1
 pen_ineq = pen * 0.5*(sign(1 - sq_norm(phi.dx(0)))+1) * inner(phi_t.dx(1), psi.dx(1)) * dx
 a += pen_ineq
 
@@ -130,6 +149,9 @@ fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 for i in vec_phi_aux:
   ax.scatter(i[0], i[1], i[2], color='r')
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('z')
 plt.show()
 plt.title('Miura ori')
 plt.savefig('new_shape_%i.pdf' % num_computation)
