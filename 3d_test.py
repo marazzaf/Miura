db04c0b2788027bc6a64cbcda76d98b6b56acbc0
marazzaf <frederic.marazzato@ponts.org>
2022-01-14 12:08:53 -0600

    Make x,y,z 2d and that whould work.

--- 3d_test.py
+++ 3d_test.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 import sys
-import open3d as o3d
+from mayavi import mlab
 
 size_ref = 20
 #data = np.loadtxt('hyperboloid_%i.txt' % size_ref)
@@ -14,33 +14,12 @@ y = data[:,1]
 z = data[:,2]
 
 #test new plot
-points = np.vstack((x, y, z)).transpose()
-pcd = o3d.geometry.PointCloud()
-pcd.points = o3d.utility.Vector3dVector(points)
-o3d.visualization.draw_geometries([pcd])
-sys.exit()
-
-#creating a mesh
-#msh = o3d.geometry.TriangleMesh()
-#for p in data:
-#    msh.vertices.append(p) #adding vertices to the mesh
-
-#add the normals now
-normals = np.loadtxt('normals_%i.txt' % size_ref)
-for n in normals:
-    pcd.normals.append(n) #adding normals to the point cloud
-o3d.visualization.draw_geometries([pcd], point_show_normal=True)
-sys.exit()
-
-#Constructing the mesh
-with o3d.utility.VerbosityContextManager(o3d.utility.VerbosityLevel.Debug) as cm:
-    mesh, densities = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(pcd, depth=9)
-#o3d.visualization.draw_geometries([mesh])
-sys.exit()
-
-#try to get triangular surface
-radii = [0.005, 0.01, 0.02, 0.04]
-rec_mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_ball_pivoting(pcd, o3d.utility.DoubleVector(radii))
-o3d.visualization.draw_geometries([pcd, rec_mesh])
-
-
+mlab.clf()
+phi, theta = np.mgrid[0:np.pi:11j, 0:2*np.pi:11j]
+x = np.sin(phi) * np.cos(theta)
+y = np.sin(phi) * np.sin(theta)
+z = np.cos(phi)
+mlab.mesh(x, y, z)
+mlab.mesh(x, y, z, representation='wireframe', color=(0, 0, 0))
+#mlab.mesh(x, y, z)
+#mlab.mesh(x, y, z, representation='wireframe', color=(0, 0, 0))
