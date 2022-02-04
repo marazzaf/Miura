2d3af9016bfbd358cf8f0d821e9dd0b5d0fbe27a
marazzaf <frederic.marazzato@ponts.org>
2022-02-04 13:58:59 -0600

    Updated for vector computations. Try to get the shape.

--- weired_hyper/test.py
+++ weired_hyper/test.py
@@ -7,29 +7,29 @@ import numpy as np
 size_ref = 3
 L,H = 2,1
 mesh = PeriodicRectangleMesh(size_ref, size_ref, L, H, direction='y', diagonal='crossed')
-V = FunctionSpace(mesh, 'CG', 1)
+V = VectorFunctionSpace(mesh, 'CG', 1, dim=3)
 f = Function(V, name='ref')
 x = SpatialCoordinate(mesh)
-f.interpolate(exp(x[0]+x[1]))
+f.interpolate(as_vector((exp(x[0]+x[1]), 1, 2)))
 file = File('test_1.pvd')
 file.write(f)
 
 def func(data):
-    res = np.zeros(len(data))
-    for i,d in enumerate(data):
-        #print(i,d)
-        res[i] = f(d)
+    res = np.zeros((len(data),3))
+    for i,dat in enumerate(data):
+        res[i,:] = f(dat)
     return res
 
 #new mesh
 mesh = RectangleMesh(size_ref, size_ref, L, H, diagonal='crossed')
 W = VectorFunctionSpace(mesh, 'CG', 1)
 X = interpolate(mesh.coordinates, W)
-print(X.dat.data_ro.shape)
-print('***************************')
-print(func(X.dat.data_ro).shape)
-V = FunctionSpace(mesh, 'CG', 1)
+#print(X.dat.data_ro.shape)
+#print('***************************')
+#print(func(X.dat.data_ro).shape)
+V = VectorFunctionSpace(mesh, 'CG', 1, dim=3)
 F = Function(V, name='interpolated')
+#print(F.dat.data[:].shape)
 F.dat.data[:] = func(X.dat.data_ro)
 file = File('test_2.pvd')
 file.write(F)
