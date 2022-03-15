46fc25d9bebd17404812b1f8b4138fc192443c09
marazzato <marazzato@system76-pc.localdomain>
2022-03-15 16:08:57 -0500

    Corrected the initial guess problem for the convergence script.

--- hyperboloid/convergence.py
+++ hyperboloid/convergence.py
@@ -26,9 +26,8 @@ l = sin(theta/2)*L
 #Creating mesh
 size_ref = 50 #10 #degub: 5
 #mesh = RectangleMesh(size_ref, size_ref, L, H)
-mesh = Mesh('convergence_1.msh')
-#V = VectorFunctionSpace(mesh, "ARG", 5, dim=3)
-V = VectorFunctionSpace(mesh, "BELL", 5, dim=3) #faster
+mesh = Mesh('mesh/convergence_1.msh')
+V = VectorFunctionSpace(mesh, "BELL", 5, dim=3)
 PETSc.Sys.Print('Nb dof: %i' % V.dim())
 
 #For projection
@@ -41,32 +40,33 @@ z = 2*sin(theta/2) * (x[0]-L/2)
 phi_D = as_vector((rho*cos(alpha*x[1]), rho*sin(alpha*x[1]), z))
 
 #initial guess
-#solve laplace equation on the domain
-phi = Function(V, name='solution')
-phi_t = TrialFunction(V)
-psi = TestFunction(V)
+W = VectorFunctionSpace(mesh, 'CG', 4, dim=3)
+phi_l = Function(W, name='solution')
+phi_t = TrialFunction(W)
+psi = TestFunction(W)
 laplace = inner(grad(phi_t), grad(psi)) * dx #laplace in weak form
-#penalty term for Dirichlet BC
-h = CellDiameter(mesh)
-pen = 1e1 #1e2
-pen_term = pen/h**4 * inner(phi_t, psi) * ds #(ds(1) + ds(2))
-L = pen/h**4 * inner(phi_D, psi)  * ds #(ds(1) + ds(2))
-#pen_term = pen * inner(phi_t, psi) * ds #(ds(1) + ds(2))
-#L = pen * inner(phi_D, psi)  * ds #(ds(1) + ds(2))
-A = assemble(laplace+pen_term)
-b = assemble(L)
-solve(A, phi, b, solver_parameters={'direct_solver': 'mumps'})
-#solve(A, phi, b, solver_parameters={'ksp_type': 'cg','pc_type': 'bjacobi', 'ksp_rtol': 1e-5})
+L = inner(Constant((0,0,0)), psi) * dx
+bcs = [DirichletBC(W, phi_D, 1), DirichletBC(W, phi_D, 2), DirichletBC(W, phi_D, 3), DirichletBC(W, phi_D, 4)]
+#solve laplace equation on the domain
+A = assemble(laplace, bcs=bcs)
+b = assemble(L, bcs=bcs)
+solve(A, phi_l, b, solver_parameters={'direct_solver': 'mumps'})
 PETSc.Sys.Print('Laplace equation ok')
 
 #Writing our problem now
+phi = Function(V, name='solution')
+phi.vector()[:] = project(phi_l, V).vector()
+phi_t = TrialFunction(V)
+psi = TestFunction(V)
 #bilinear form for linearization
 a = inner(p(phi) * phi_t.dx(0).dx(0) + q(phi)*phi_t.dx(1).dx(1), div(grad(psi))) * dx
 
 #penalty to impose Dirichlet BC
-#pen_term = pen/h**4 * inner(phi_t, psi) * ds
+h = CellDiameter(mesh)
+pen = 1e1 #1e1
+pen_term = pen/h**4 * inner(phi_t, psi) * ds
 a += pen_term
-#L = pen/h**4 * inner(phi_D, psi)  * ds
+L = pen/h**4 * inner(phi_D, psi)  * ds
 
 # Picard iteration
 tol = 1e-5 #1e-9
@@ -78,7 +78,6 @@ for iter in range(maxiter):
   b = assemble(L)
   pp = interpolate(p(phi), UU)
   PETSc.Sys.Print('Min of p: %.3e' % pp.vector().array().min())
-  #solve(A, phi, b, solver_parameters={'ksp_type': 'cg','pc_type': 'bjacobi', 'ksp_rtol': 1e-5})
   solve(A, phi, b, solver_parameters={'direct_solver': 'mumps'}) # compute next Picard iterate
     
   eps = sqrt(assemble(inner(div(grad(phi-phi_old)), div(grad(phi-phi_old)))*dx)) # check increment size as convergence test
