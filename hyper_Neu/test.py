b256120505dab451d6af332f6d8d1172fa7e9658
marazzato <marazzato@system76-pc.localdomain>
2022-03-17 09:27:44 -0500

    Seems like imposing Dirichlet in normal component and the norm of the tagential vectors works. But convergence is very slow. Why?

--- hyper_Neu/test.py
+++ hyper_Neu/test.py
@@ -25,7 +25,7 @@ H = 2*pi/alpha #height of rectangle
 l = sin(theta/2)*L
 
 #Creating mesh
-size_ref = 10 #10 #degub: 5
+size_ref = 80 #10 #degub: 5
 mesh = PeriodicRectangleMesh(size_ref, size_ref, L, H, direction='y', diagonal='crossed')
 V = VectorFunctionSpace(mesh, "BELL", 5, dim=3) #faster
 VV = FunctionSpace(mesh, 'CG', 4)
@@ -73,10 +73,12 @@ a = inner(p(phi) * phi_t.dx(0).dx(0) + q(phi)*phi_t.dx(1).dx(1), div(grad(psi)))
 #test
 phi_x = phi_D.dx(0) / sqrt(inner(phi_D.dx(0),phi_D.dx(0)))
 phi_y = phi_D.dx(1) / sqrt(inner(phi_D.dx(1),phi_D.dx(1)))
+n = cross(phi_D.dx(0), phi_D.dx(1))
+n /= sqrt(inner(n, n))
 phi_aux = Function(V, name='test')
-pen = 10 * dot(phi_t.dx(0), psi.dx(0)) * (ds(1) + ds(2)) + 10 * dot(phi_t.dx(1), psi.dx(1)) * (ds(1) + ds(2))
+pen = 10 * dot(phi_t.dx(0), psi.dx(0)) * (ds(1) + ds(2)) + 10 * dot(phi_t.dx(1), psi.dx(1)) * (ds(1) + ds(2)) + 10 * dot(phi_t, n) * dot(psi, n) * (ds(1) + ds(2))
 A = assemble(a + pen)
-L = 10 * dot(phi_x, psi.dx(0)) * (ds(1) + ds(2)) + 10 * dot(phi_y, psi.dx(1)) * (ds(1) + ds(2))
+L = 10 * dot(phi_x, psi.dx(0)) * (ds(1) + ds(2)) + 10 * dot(phi_y, psi.dx(1)) * (ds(1) + ds(2)) + 10 * dot(phi_D, n) * dot(psi, n) * (ds(1) + ds(2))
 b = assemble(L)
 solve(A, phi_aux, b, solver_parameters={'direct_solver': 'mumps'})
 
