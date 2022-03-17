4e77d04c2440a879dd2a2289b0733fb52b9b265c
marazzato <marazzato@system76-pc.localdomain>
2022-03-17 18:29:47 -0500

    Convergence of fixed points is a little slow but it works! We find the hyperboloid from the minimal surface with the new BC. Check in other cases if we have (10b) and (10c) in the entire domain.

--- hyper_Neu/test_C0.py
+++ hyper_Neu/test_C0.py
@@ -46,7 +46,7 @@ solve(A, phi, b, solver_parameters={'direct_solver': 'mumps'})
 PETSc.Sys.Print('Laplace equation ok')
 
 #Writing our problem now
-phi.interpolate(phi_D)
+#phi.interpolate(phi_D)
 
 #bilinear form for linearization
 a = inner(p(phi) * phi_t.dx(0).dx(0) + q(phi)*phi_t.dx(1).dx(1), div(grad(psi))) * dx
@@ -58,7 +58,7 @@ pen_term = pen/h_avg**2 * inner(jump(grad(phi_t)), jump(grad(psi))) * dS - inner
 a += pen_term
 
 #BC to prevent rigid body rotation
-bc = [DirichletBC(V, phi_D, 5)]
+bcs = [DirichletBC(V, phi_D, 5)]
 
 #Forms to impose the BC
 phi_x = phi_D.dx(0)
@@ -66,11 +66,11 @@ phi_y = phi_D.dx(1)
 #n = cross(phi_D.dx(0), phi_D.dx(1))
 #n /= sqrt(inner(n, n))
 pen = 10
-pen_term = pen/h**2 * dot(phi_t.dx(0), psi.dx(0)) * ds + pen/h**2 * dot(phi_t.dx(1), psi.dx(1)) * ds # + pen/h**2 * dot(phi_t, n) * dot(psi, n) * ds
-a += pen_term
+pen_term_2 = pen/h**2 * dot(phi_t.dx(0), psi.dx(0)) * ds + pen/h**2 * dot(phi_t.dx(1), psi.dx(1)) * ds # + pen/h**2 * dot(phi_t, n) * dot(psi, n) * ds
+a += pen_term_2
 L = pen/h**2 * dot(phi.dx(0), psi.dx(0)) * ds + pen/h**2 * dot(phi.dx(1), psi.dx(1)) * ds # + pen/h**2 * dot(phi_D, n) * dot(psi, n) * ds
-#pen_scal = 
-#a += pen_scal
+pen_scal = pen * dot(phi.dx(0), phi.dx(1)) * (dot(phi_t.dx(0), psi.dx(1)) + dot(phi_t.dx(1), psi.dx(0))) * ds
+a += pen_scal
 
 # Picard iteration
 tol = 1e-5 #1e-9
@@ -78,8 +78,8 @@ maxiter = 50
 phi_old = Function(V) #for iterations
 for iter in range(maxiter):
   #linear solve
-  A = assemble(a, bcs=bc)
-  b = assemble(L, bcs=bc)
+  A = assemble(a, bcs=bcs)
+  b = assemble(L, bcs=bcs)
   solve(A, phi, b, solver_parameters={'direct_solver': 'mumps'}) # compute next Picard iterate
     
   eps = sqrt(assemble(inner(div(grad(phi-phi_old)), div(grad(phi-phi_old)))*dx)) # check increment size as convergence test
@@ -95,25 +95,19 @@ else:
   PETSc.Sys.Print('convergence after {} Picard iterations'.format(iter+1))
 
 #Computing error
-X = VectorFunctionSpace(mesh, 'CG', 2, dim=3)
-projected = interpolate(div(grad(phi)), X)
-ref = interpolate(div(grad(phi_D)), X)
 err = sqrt(assemble(inner(div(grad(phi-phi_D)), div(grad(phi-phi_D)))*dx))
 PETSc.Sys.Print('Error: %.3e' % err)
 
 #Write 3d results
 file = File('hyper.pvd')
 x = SpatialCoordinate(mesh)
-projected = Function(U, name='surface')
+projected = Function(V, name='surface')
 projected.interpolate(phi - as_vector((x[0], x[1], 0)))
 file.write(projected)
 
 #Write 2d result
 file_bis = File('flat.pvd')
-#proj = project(phi, U, name='flat')
-proj = Function(U, name='flat')
-proj.interpolate(phi - 1.e-5*as_vector((x[0], x[1], 0)))
-file_bis.write(proj)
+file_bis.write(phi)
 
 sys.exit()
 
