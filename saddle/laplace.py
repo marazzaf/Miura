8fd0cc982a75ad3b6d127ba8d09ca2f07aa37f02
marazzaf <frederic.marazzato@ponts.org>
2022-06-22 10:34:39 -0500

    Tried to apply the weired BC on the laplacian. Does not seem to work. What next?

--- saddle/laplace.py
+++ saddle/laplace.py
@@ -6,44 +6,26 @@ from firedrake.petsc import PETSc
 import sys
 import numpy as np
 
-# the coefficient functions
-def p(phi):
-  sq = inner(phi.dx(0), phi.dx(0))
-  aux = 4 / (4 - sq)
-  truc = conditional(gt(sq, Constant(3)), Constant(4), aux)
-  return truc
-
-def q(phi):
-  sq = inner(phi.dx(1), phi.dx(1))
-  aux = 4 / sq
-  truc = conditional(lt(sq, Constant(1)), Constant(4), aux)
-  truc2 = conditional(gt(sq, Constant(4)), Constant(1), truc)
-  return truc2
-
 # Create mesh and define function space
 LL = 2 #length of rectangle
 H = 1 #height of rectangle
 mesh= Mesh('mesh_1.msh')
 size_ref = 1
-V = VectorFunctionSpace(mesh, "BELL", 5, dim=3)
+V = VectorFunctionSpace(mesh, "CG", 2, dim=3)
 PETSc.Sys.Print('Nb dof: %i' % V.dim())
-
-#For projection
-W = VectorFunctionSpace(mesh, 'CG', 4, dim=3)
-UU = FunctionSpace(mesh, 'CG', 4)
 
 # Boundary conditions
 x = SpatialCoordinate(mesh)
 n = FacetNormal(mesh)
-aux = x[0]+x[1] #x[0]**2 + x[1]**2
+aux = x[0]**2 + x[1]**2
 g1 = conditional(gt(aux, Constant(3)), Constant(3), aux)
 g2 = 4 / (4 - g1)
-g = as_vector((g1, g2, 0)) #g1))
+g = as_vector((g1, g2, g1))
 
 # Creating function to store solution
 phi = Function(V, name='solution')
 phi_old = Function(V) #for iterations
-phi.project(as_vector((x[0],-x[1],x[0])))
+phi.interpolate(as_vector((x[0],-x[1],x[0])))
 #Defining the bilinear forms
 #bilinear form for linearization
 phi_t = TrialFunction(V)
@@ -58,67 +40,42 @@ pen_term = pen * inner(B_t, B) * ds #(ds(5)+ds(11)+ds(8)+ds(6))
 L = pen * inner(g, B) * ds #(ds(5)+ds(11)+ds(8)+ds(6))
 
 ##penalty for Neumann BC
-#pen_term = pen * inner(dot(grad(phi_t),n), dot(grad(psi),n)) * ds #(ds(5)+ds(11)+ds(8)+ds(6))
-#L = pen * inner(g, dot(grad(psi),n)) * ds #(ds(5)+ds(11)+ds(8)+ds(6))
+pen_term = pen * inner(dot(grad(phi_t),n), dot(grad(psi),n)) * ds #(ds(5)+ds(11)+ds(8)+ds(6))
+L = pen * inner(g, dot(grad(psi),n)) * ds #(ds(5)+ds(11)+ds(8)+ds(6))
 
-#penalty terms so solution can't move
-#for translation
-pen_disp = pen/h**4 * inner(phi_t,psi) * ds(1)
-#for directions
+#Bilinear form
+laplace = inner(grad(phi_t), grad(psi)) * dx #laplace in weak form
+#laplace = inner(div(grad(phi_t)), div(grad(psi))) * dx #test
+a = laplace+pen_term
+
+#BC to have uniqueness
 tau_4 = Constant((1,0,0))
 pen_rot = pen/h**4 * inner(phi_t,tau_4) * inner(psi,tau_4)  * ds(4) #e_z blocked
 tau_3 = Constant((0,0,1))
 pen_rot += pen/h**4 * inner(phi_t,tau_3) * inner(psi,tau_3)  * ds(3) #e_x blocked
 tau_2 = Constant((0,0,1))
 pen_rot += pen/h**4 * inner(phi_t,tau_2) * inner(psi,tau_2)  * ds(2) #e_y blocked
+bcs = [DirichletBC(V, Constant((0,0,0)), 1), DirichletBC(V.sub(0), Constant(0), 4), DirichletBC(V.sub(2), Constant(0), 3), DirichletBC(V.sub(2), Constant(0), 2)]
 
-#Computing initial guess
-laplace = inner(grad(phi_t), grad(psi)) * dx #laplace in weak form
-#laplace = inner(div(grad(phi_t)), div(grad(psi))) * dx #test
-A = assemble(laplace+pen_term+pen_disp+pen_rot)
-b = assemble(L)
-solve(A, phi, b, solver_parameters={'direct_solver': 'mumps'})
-PETSc.Sys.Print('Laplace equation ok')
-
-file_bis = File('laplacian.pvd')
-proj = project(phi - as_vector((x[0],x[1],0)), W, name='laplacian')
-file_bis.write(proj)
-#sys.exit()
-
-#Bilinear form
-Gamma = (p(phi) + q(phi)) / (p(phi)*p(phi) + q(phi)*q(phi)) 
-a = Gamma * inner(p(phi) * phi_t.dx(0).dx(0) + q(phi)*phi_t.dx(1).dx(1), div(grad(psi))) * dx
-a += pen_term + pen_disp +pen_rot
-
-file = File('res.pvd')
+file = File('res_laplace.pvd')
 
 # Picard iterations
 tol = 1e-5 #1e-9
 maxiter = 50
 for iter in range(maxiter):
   #linear solve
-  A = assemble(a)
-  b = assemble(L)
+  A = assemble(a, bcs=bcs)
+  b = assemble(L, bcs=bcs)
   solve(A, phi, b, solver_parameters={'direct_solver': 'mumps'}) # compute next Picard iterate
   
   #convergence test 
-  eps = sqrt(assemble(inner(div(grad(phi-phi_old)), div(grad(phi-phi_old)))*dx)) # check increment size as convergence test
-  PETSc.Sys.Print('iteration{:3d}  H2 seminorm of delta: {:10.2e}'.format(iter+1, eps))
+  eps = sqrt(assemble(inner(grad(phi-phi_old), grad(phi-phi_old))*dx)) # check increment size as convergence test
+  PETSc.Sys.Print('iteration{:3d}  H1 seminorm of delta: {:10.2e}'.format(iter+1, eps))
   #output
-  projected = Function(W, name='surface')
+  projected = Function(V, name='surface')
   projected.interpolate(phi - as_vector((x[0], x[1], 0)))
   file.write(projected)
 
-  #assert on disp and directions?
-  projected.interpolate(phi - 1e-5*as_vector((x[0], x[1], 0)))
-  blocked = projected.at((0,0))
-  assert np.linalg.norm(blocked) < 0.05 * abs(projected.vector()[:].max()) #checking that the disp at the origin is blocked
-  blocked = projected.at((LL-1e-5,0))[2]
-  assert abs(blocked) < 0.05 * abs(projected.vector()[:].max())
-  blocked = projected.at((LL-1e-5,H-1e-5))[2]
-  assert abs(blocked) < 0.05 * abs(projected.vector()[:].max())
-  blocked = projected.at((0,H-1e-5))[0]
-  assert abs(blocked) < 0.05 * abs(projected.vector()[:].max())
   if eps < tol:
     break
   phi_old.assign(phi)
@@ -127,6 +84,8 @@ if eps > tol:
   PETSc.Sys.Print('no convergence after {} Picard iterations'.format(iter+1))
 else:
   PETSc.Sys.Print('convergence after {} Picard iterations'.format(iter+1))
+
+sys.exit()
 
 #Write 2d results
 flat = File('flat_%i.pvd' % size_ref)
---
+++ saddle/mesh.geo
@@ -0,0 +1,46 @@
+//Définition des valeurs physiques
+L = 2; //Longeur selon x
+H = 1; //Longuer selon y
+h = 0.05; //0.05; //0.01; //Taille du maillage
+aux = h/5;
+
+Point(1) = {0,0,0,h};
+Point(2) = {aux,0,0,h};
+Point(3) = {0,aux,0,h};
+Point(4) = {0,H,0,h};
+Point(5) = {0,H-aux,0,h};
+Point(6) = {aux,H,0,h};
+Point(7) = {L,0,0,h};
+Point(8) = {L-aux,0,0,h};
+Point(9) = {L,aux,0,h};
+Point(10) = {L,H,0,h};
+Point(11) = {L-aux,H,0,h};
+Point(12) = {L,H-aux,0,h};
+
+Line(1) = {1,2};
+Line(2) = {2,8};
+Line(3) = {8,7};
+Line(4) = {7,9};
+Line(5) = {9,12};
+Line(6) = {12,10};
+Line(7) = {10,11};
+Line(8) = {11,6};
+Line(9) = {6,4};
+Line(10) = {4,5};
+Line(11) = {5,3};
+Line(12) = {3,1};
+
+Line Loop(1) = {1:12};
+
+Plane Surface(1) = {1};
+
+Physical Line(1) = {1,12};
+Physical Line(2) = {3,4};
+Physical Line(3) = {6,7};
+Physical Line(4) = {9,10};
+Physical Line(5) = {5};
+Physical Line(11) = {11};
+Physical Line(8) = {8};
+Physical Line(6) = {2};
+
+Physical Surface(1) = {1};
