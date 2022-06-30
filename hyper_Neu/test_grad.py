904ba5a4ccada6d2b40e97178795298c5fbc9ca9
marazzaf <frederic.marazzato@ponts.org>
2022-06-30 10:59:32 -0500

    Just solving the laplacian for now. Go to the real equation now.

--- hyper_Neu/test_grad.py
+++ hyper_Neu/test_grad.py
@@ -42,7 +42,7 @@ phi_ref = as_vector((rho*cos(alpha*x[1]), rho*sin(alpha*x[1]), z))
 
 #initial guess
 #solve laplace equation on the domain
-g_phi = Function(V, name='solution')
+g_phi = Function(V, name='grad solution')
 g_phi_t = TrialFunction(V)
 g_psi = TestFunction(V)
 laplace = inner(grad(g_phi_t), grad(g_psi)) * dx #laplace in weak form
@@ -58,26 +58,15 @@ solve(A, g_phi, b, solver_parameters={'direct_solver': 'mumps'})
 #solve(A, phi, b, solver_parameters={'ksp_type': 'cg','pc_type': 'bjacobi', 'ksp_rtol': 1e-5})
 PETSc.Sys.Print('Laplace equation ok')
 
-#Reconstructing phi
-W = VectorFunctionSpace(mesh, "CG", 3, dim=3)
-phi = Function(W, name='solution')
-phi_t = TrialFunction(W)
-aux = inner(grad(phi_t), g_psi) * dx
-L = inner(g_phi, g_psi) * dx
-
-#BC for now. Make zero average later
-bcs_test = [DirichletBC(W, phi_ref, 1), DirichletBC(W, phi_ref, 2)]
-
-#solving
-A = assemble(A) #, bcs=bcs_test)
-b = assemble(L) #, bcs=bcs_test)
-solve(A, phi, b, solver_parameters={'direct_solver': 'mumps'})
-
 #Write 3d results
 file = File('laplacian.pvd')
-projected = Function(W, name='surface')
-projected.interpolate(phi - as_vector((x[0], x[1], 0)))
-file.write(projected)
+file.write(g_phi)
+
+#ref
+file = File('ref.pvd')
+ref = Function(V, name='grad ref')
+ref.interpolate(grad(phi_ref))
+file.write(ref)
 sys.exit()
 
 #Writing our problem now
@@ -98,6 +87,12 @@ L = pen/h**4 * inner(phi_ref, psi) * (ds(1) + ds(2))
 
 # Solving with Newton method
 solve(a+pen_term - L == 0, phi, solver_parameters={'snes_monitor': None})
+
+#Computing the error
+err = sqrt(assemble(inner(div(grad(phi-phi_ref)), div(grad(phi-phi_ref)))*dx))
+PETSc.Sys.Print('Error: %.3e' % err)
+sys.exit()
+
 file = File('res.pvd')
 projected = Function(U, name='surface')
 projected.interpolate(phi - as_vector((x[0], x[1], 0)))
@@ -129,31 +124,10 @@ if eps > tol:
 else:
   PETSc.Sys.Print('convergence after {} Picard iterations'.format(iter+1))
 
-sys.exit()
-#For projection
-file = File('res.pvd')
-projected = Function(U, name='surface')
-#projected.interpolate(phi - as_vector((x[0], x[1], 0)))
-file.write(projected)
-sys.exit()
-
 #Computing error
 X = VectorFunctionSpace(mesh, 'CG', 2, dim=3)
 projected = interpolate(div(grad(phi)), X)
 ref = interpolate(div(grad(phi_ref)), X)
 err = sqrt(assemble(inner(div(grad(phi-phi_ref)), div(grad(phi-phi_ref)))*dx))
-#PETSc.Sys.Print('Error: %.3e' % err)
-
-#Tests if inequalities are true
-file_bis = File('verif_x.pvd')
-phi_x = interpolate(phi.dx(0), U)
-proj = project(inner(phi_x,phi_x), UU, name='test phi_x')
-file_bis.write(proj)
-file_ter = File('verif_y.pvd')
-phi_y = interpolate(phi.dx(1), U)
-proj = project(inner(phi_y,phi_y), UU, name='test phi_y')
-file_ter.write(proj)
-file_4 = File('verif_prod.pvd')
-proj = project(inner(phi_x,phi_y), UU, name='test PS')
-file_4.write(proj)
+PETSc.Sys.Print('Error: %.3e' % err)
 
