28bee9d2dd794f8c73ef65a6143e0c5ec2f489ee
marazzato <marazzato@system76-pc.localdomain>
2023-01-14 11:38:56 -0600

    Adding the ref solution for a new test case.

---
+++ Mixed/axy/ref.py
@@ -0,0 +1,21 @@
+import numpy as np
+from scipy.integrate import solve_ivp
+import matplotlib.pyplot as plt
+
+#def exponential_decay(t, y): return -0.5 * y
+#
+#sol = solve_ivp(exponential_decay, [0, 10], [2, 4, 8])
+#
+#print(sol.t)
+#print(sol.y)
+
+alpha = 1
+
+def rhs(t, y):
+    aux = 4*alpha*alpha * y[0] / (4 - alpha*alpha*y[0]*y[0])**2
+    return [y[1], aux]
+
+sol = solve_ivp(rhs, [0, 10], [0, 0.5])
+
+plt.plot(sol.t, sol.y[0], '*-')
+plt.show()
