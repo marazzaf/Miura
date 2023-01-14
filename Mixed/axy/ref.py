30e4154317d252dac1f46ea9cbc27569f5c6a034
marazzato <marazzato@system76-pc.localdomain>
2023-01-14 17:51:19 -0600

    Computed the BC. Difficult to plot though. Use pyvista to make sure that ok?

--- Mixed/axy/ref.py
+++ Mixed/axy/ref.py
@@ -15,9 +15,37 @@ def rhs(t, y):
     aux = 4*alpha*alpha * y[0] / (4 - alpha*alpha*y[0]*y[0])**2
     return [y[1], aux]
 
-rho = solve_ivp(rhs, [0, 10], [0, 0.5])
+N = 10
+L = 2*np.pi/alpha
+H = 10
+
+beta_0 = np.pi/3
+theta_0 = np.pi/2
+rho_0 = 0
+rho_p_0 = 2*np.sin(beta_0)*np.cos(theta_0/2)
+rho = solve_ivp(rhs, [0, H], [rho_0, rho_p_0], max_step=H/N)
 
 plt.plot(rho.t, rho.y[0], '*-')
 plt.show()
 
-L = 2*np.pi/alpha
+M = len(rho.y[0])
+print(M)
+
+aux = np.cos(beta_0)/np.cos(theta_0/2)
+Z = np.arange(0, aux*H, aux*H/M)
+print(len(Z))
+X = np.arange(0, L, L/M)
+print(len(X))
+
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+
+for r,z in zip(rho.y[0],Z):
+    for x in X:
+        ax.scatter(r*np.cos(alpha*x), r*np.sin(alpha*x), z)
+
+ax.set_xlabel('X Label')
+ax.set_ylabel('Y Label')
+ax.set_zlabel('Z Label')
+
+plt.show()
